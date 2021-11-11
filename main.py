41e14a9439f32d16eaa1c876ddb7670eaa54e79a
anthonyyoussef01 <30884253+anthonyyoussef01@users.noreply.github.com>
2021-11-11 17:07:01 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+# Drawing
+import gizeh as gz
+# Compiling a video
+import moviepy.editor as mpy
+# Youtube Data API
+import datetime
+from Google import Create_Service
+from googleapiclient.http import MediaFileUpload
+
+if __name__ == '__main__':
+  ...
